560496f1914722276b72257392781c3d30c044b3
Gál Kristóf <gal.kristof08@gmail.com>
2023-01-04 15:24:07 +0100

    Component, HTML, Modules alapok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +22,17 @@ import { TodoComponent } from './todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatFormFieldModule, 
+    MatSelectModule, 
+    MatOptionModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+import { ToDo } from '../models/todo.model';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit{
+  
+  todo?: ToDo;
+  isEdit = false;
+
+  minDate = new Date(); // mai napi dátum. Nem lehet múltba létrehozni ToDot.
+  maxDate = new Date(2040, 0, 1); // max dátum 2040 jan 1.
+  
+  todoForm: FormGroup = new FormGroup({
+    title: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    status: new FormControl('Nincs elkezdve', Validators.required),
+    priority: new FormControl('Normál', Validators.required),
+    deadline: new FormControl('', Validators.required)
+  });
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private todoService: TodoService
+  ){};
+    
+  ngOnInit(): void {
+  }
+
+  onDelete(){};
+  onSubmit(){};
+
 
 }
